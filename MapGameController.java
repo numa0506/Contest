d18d83adb03693f695349afc687c6106ab93d055
KotaNaganuma <naganuma.kota.19@shizuoka.ac.jp>
2020-01-08 22:52:52 +0900

    音声実装ファイル受け取り
    正常に動作しているのかは知らん

--- MapGameController.java
+++ MapGameController.java
@@ -11,144 +11,139 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.Group;
 
 public class MapGameController implements Initializable {
-  public MapData mapData;
-  public MoveChara chara;
-  public GridPane mapGrid;
-  public ImageView[] mapImageViews;
-  //    public Group[] mapGroups;
+    public MapData mapData;
+    public MoveChara chara;
+    public GridPane mapGrid;
+    public ImageView[] mapImageViews;
+//    public Group[] mapGroups;
 
-  @Override
-  public void initialize(URL url, ResourceBundle rb) {
-    mapData = new MapData(21,15);
-    chara = new MoveChara(1,1,mapData);
-    //        mapGroups = new Group[mapData.getHeight()*mapData.getWidth()];
-    mapImageViews = new ImageView[mapData.getHeight()*mapData.getWidth()];
-    for(int y=0; y<mapData.getHeight(); y++){
-      for(int x=0; x<mapData.getWidth(); x++){
-        int index = y*mapData.getWidth() + x;
-        mapImageViews[index] = mapData.getImageView(x,y);
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        mapData = new MapData(21,15);
+        chara = new MoveChara(1,1,mapData);
+//        mapGroups = new Group[mapData.getHeight()*mapData.getWidth()];
+        mapImageViews = new ImageView[mapData.getHeight()*mapData.getWidth()];
+        for(int y=0; y<mapData.getHeight(); y++){
+            for(int x=0; x<mapData.getWidth(); x++){
+                int index = y*mapData.getWidth() + x;
+                mapImageViews[index] = mapData.getImageView(x,y);
+            }
+        }
+        mapPrint(chara,mapData);
       }
+
+    public void resetMapIfGoal(){//Restart MapGame if catch GOAL
+      if (chara.goal()){
+        mapData = new MapData(21,15);
+        chara = new MoveChara(1,1,mapData);
+//        mapGroups = new Group[mapData.getHeight()*mapData.getWidth()];
+        mapImageViews = new ImageView[mapData.getHeight()*mapData.getWidth()];
+        for(int y=0; y<mapData.getHeight(); y++){
+            for(int x=0; x<mapData.getWidth(); x++){
+                int index = y*mapData.getWidth() + x;
+                mapImageViews[index] = mapData.getImageView(x,y);
+            }
+        }
+        mapPrint(chara, mapData);
     }
-    mapPrint(chara,mapData);
   }
 
-  public void resetMapIfGoal(){//Restart MapGame if catch GOAL
-    if(chara.goal()){
-      mapData = new MapData(21,15);
-      chara = new MoveChara(1,1,mapData);
-      //           mapGroups = new Group[mapData.getHeight()*mapData.getWidth()];
-      mapImageViews = new ImageView[mapData.getHeight()*mapData.getWidth()];
+    public void imageRestore(){
       for(int y=0; y<mapData.getHeight(); y++){
-        for(int x=0; x<mapData.getWidth(); x++){
-          int index = y*mapData.getWidth() + x;
-          mapImageViews[index] = mapData.getImageView(x,y);
+          for(int x=0; x<mapData.getWidth(); x++){
+              int index = y*mapData.getWidth() + x;
+              mapImageViews[index] = mapData.getImageView(x,y);
+          }
+      }
+    }
+
+
+    public void mapPrint(MoveChara c, MapData m){
+        imageRestore();
+        int cx = c.getPosX();
+        int cy = c.getPosY();
+        mapGrid.getChildren().clear();
+        for(int y=0; y<mapData.getHeight(); y++){
+            for(int x=0; x<mapData.getWidth(); x++){
+                int index = y*mapData.getWidth() + x;
+                if (x==cx && y==cy) {
+                    mapGrid.add(c.getCharaImageView(), x, y);
+                } else {
+                    mapGrid.add(mapImageViews[index], x, y);
+                }
+            }
         }
-      }
-      mapPrint(chara, mapData);
     }
-  } //新たにインスタンスを生成しなおし、マップとキャラクターを表示するメソッド
 
-  public void imageRestore(){ //地図データをMapDataクラスから読み込み直す
-    for(int y=0; y<mapData.getHeight(); y++){
-      for(int x=0; x<mapData.getWidth(); x++){
-        int index = y*mapData.getWidth() + x;
-        mapImageViews[index] = mapData.getImageView(x,y);
-      }
-    }
-  }
+    public void func1ButtonAction(ActionEvent event) { }
+    public void func2ButtonAction(ActionEvent event) { }
+    public void func3ButtonAction(ActionEvent event) { }
+    public void func4ButtonAction(ActionEvent event) { }
 
-
-  public void mapPrint(MoveChara c, MapData m){
-    imageRestore();
-    int cx = c.getPosX();
-    int cy = c.getPosY();
-    mapGrid.getChildren().clear();
-    for(int y=0; y<mapData.getHeight(); y++){
-      for(int x=0; x<mapData.getWidth(); x++){
-        int index = y*mapData.getWidth() + x;
-        if (x==cx && y==cy) {
-          mapGrid.add(c.getCharaImageView(), x, y);
-        } else {
-          mapGrid.add(mapImageViews[index], x, y);
+    public void keyAction(KeyEvent event){
+        KeyCode key = event.getCode();
+        if (key == KeyCode.DOWN){
+            downButtonAction();
+        }else if (key == KeyCode.RIGHT){
+            rightButtonAction();
         }
-      }
-    }
-  }
-
-  public void func1ButtonAction(ActionEvent event) { }
-  public void func2ButtonAction(ActionEvent event) { }
-  public void func3ButtonAction(ActionEvent event) { }
-  public void func4ButtonAction(ActionEvent event) { }
-
-  public void keyAction(KeyEvent event){
-    KeyCode key = event.getCode();
-    if (key == KeyCode.DOWN){
-      downButtonAction();
-    }else if (key == KeyCode.RIGHT){
-      rightButtonAction();
-    }
-    //tuika
-    else if (key == KeyCode.LEFT){
-      leftButtonAction();
+        else if (key == KeyCode.LEFT){
+            leftButtonAction();
+        }
+        else if (key == KeyCode.UP){
+            upButtonAction();
+        }
     }
 
-    //tuika
-    else if (key == KeyCode.UP){
-      upButtonAction();
+    public void outputAction(String actionString) {
+        System.out.println("Select Action: " + actionString);
     }
-  }
 
-  public void outputAction(String actionString) {
-    System.out.println("Select Action: " + actionString);
-  }
+    public void downButtonAction(){
+        outputAction("DOWN");
+        chara.setCharaDir(MoveChara.TYPE_DOWN);
+        chara.move(0, 1);
+        mapPrint(chara, mapData);
+        resetMapIfGoal();//if it is Goal =>restart map
 
-  public void downButtonAction(){
-    outputAction("DOWN");
-    chara.setCharaDir(MoveChara.TYPE_DOWN);
-    chara.move(0, 1);
-    mapPrint(chara, mapData);
-    resetMapIfGoal();//if it is Goal =>restart map
+    }
+    public void downButtonAction(ActionEvent event) {
+        downButtonAction();
+    }
 
-  }
-  public void downButtonAction(ActionEvent event) {
-    downButtonAction();
-  }
+    public void rightButtonAction(){
+        outputAction("RIGHT");
+        chara.setCharaDir(MoveChara.TYPE_RIGHT);
+        chara.move( 1, 0);
+        mapPrint(chara, mapData);
+        resetMapIfGoal();//if it is Goal =>restart map
 
-  public void rightButtonAction(){
-    outputAction("RIGHT");
-    chara.setCharaDir(MoveChara.TYPE_RIGHT);
-    chara.move( 1, 0);
-    mapPrint(chara, mapData);
-    resetMapIfGoal();//if it is Goal =>restart map
+    }
+    public void rightButtonAction(ActionEvent event) {
+        rightButtonAction();
+    }
 
-  }
-  public void rightButtonAction(ActionEvent event) {
-    rightButtonAction();
-  }
+    public void leftButtonAction(){
+        outputAction("LEFT");
+        chara.setCharaDir(MoveChara.TYPE_LEFT);
+        chara.move( -1, 0);
+        mapPrint(chara, mapData);
+        resetMapIfGoal();//if it is Goal =>restart map
+    }
+    public void leftButtonAction(ActionEvent event) {
+        leftButtonAction();
+    }
 
-  //tuika
-  public void leftButtonAction(){
-    outputAction("LEFT");
-    chara.setCharaDir(MoveChara.TYPE_LEFT);
-    chara.move( -1, 0);
-    mapPrint(chara, mapData);
-    resetMapIfGoal();//if it is Goal =>restart map
-  }
-  public void leftButtonAction(ActionEvent event) {
-    leftButtonAction();
-  }
-
-  //tuika
-  public void upButtonAction(){
-    outputAction("UP");
-    chara.setCharaDir(MoveChara.TYPE_UP);
-    chara.move( 0, -1);
-    mapPrint(chara, mapData);
-    resetMapIfGoal();//if it is Goal =>restart map
-  }
-  public void upButtonAction(ActionEvent event) {
-    upButtonAction();
-  }
+    public void upButtonAction(){
+        outputAction("UP");
+        chara.setCharaDir(MoveChara.TYPE_UP);
+        chara.move( 0, -1);
+        mapPrint(chara, mapData);
+        resetMapIfGoal();//if it is Goal =>restart map
+    }
+    public void upButtonAction(ActionEvent event) {
+        upButtonAction();
+    }
 
 
 
