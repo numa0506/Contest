6b629688f48dd4d46f3d20eea8f163c042220aa0
Kota_Naganuma <naganuma.kota.19@shizuoka.ac.jp>
2020-01-26 10:55:40 +0900

    永沼君追加差分

--- TimerController.java
+++ TimerController.java
@@ -10,7 +10,8 @@ import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
 
 public class TimerController implements Initializable{
-    public static TimerController timerInst;
+    public static TimerController timerInst; //これは使わなければ消す
+    private int h;
     private int m;
     private int s;
     private int ms;
@@ -24,7 +25,13 @@ public class TimerController implements Initializable{
     new KeyFrame(Duration.millis(10), new EventHandler<ActionEvent>(){
         @Override
         public void handle(ActionEvent event){
-            ms = countUp(ms, 99);
+            if(ms < 99){
+                ms += 1;
+            }else{
+                ms = 0;
+                msecTimer.stop();
+                msecTimer.play();
+            }
             msecLabel.setText(castToString(ms));
         }
     }));
@@ -33,9 +40,15 @@ public class TimerController implements Initializable{
     new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>(){
         @Override
         public void handle(ActionEvent event) {
-            s = countUp(s, 59);
-            secCounter++;
+            if(s < 59){
+                s += 1;
+            }else{
+                s = 0;
+                secTimer.stop();
+                secTimer.play();
+            }
             secLabel.setText(castToString(s));
+            secCounter++;
         }
     }));
 
@@ -43,32 +56,29 @@ public class TimerController implements Initializable{
     new KeyFrame(Duration.minutes(1), new EventHandler<ActionEvent>(){
         @Override
         public void handle(ActionEvent event) {
-            m = countUp(m, 59);
-            minLabel.setText(castToString(m));
+            if(m < 59){
+                m += 1;
+                minLabel.setText(castToString(m));
+            }else{
+                m = 0;
+                h += 1;
+                minTimer.stop();
+                minTimer.play();
+                minLabel.setText("" + h + " : " + castToString(m));
+            }
         }
     }));
 
     public void initialize(URL url, ResourceBundle rb){
-        msecTimer.setCycleCount(Timeline.INDEFINITE);
-        secTimer.setCycleCount(Timeline.INDEFINITE);
-        minTimer.setCycleCount(Timeline.INDEFINITE);
+        initTimer();
         startTimer();
     }
 
-    public int countUp(int t, int limit){
-        t = (t == limit ? 0 : t + 1);
-        return t;
-    }
-
-    public String castToString(int i){
-        return String.format("%02d", i);
-    }
-
-    public TimerController getInstance(){
-        return timerInst;
-    }
-
-    public void startTimer(){
+    public void initTimer(){
+        msecTimer.setCycleCount(Timeline.INDEFINITE);
+        secTimer.setCycleCount(Timeline.INDEFINITE);
+        minTimer.setCycleCount(Timeline.INDEFINITE);
+        h = 0;
         m = 0;
         s = 0;
         ms = 0;
@@ -76,6 +86,9 @@ public class TimerController implements Initializable{
         msecLabel.setText("00");
         secLabel.setText("00");
         minLabel.setText("00");
+    }
+
+    public void startTimer(){
         msecTimer.play();
         secTimer.play();
         minTimer.play();
@@ -86,5 +99,25 @@ public class TimerController implements Initializable{
         msecTimer.stop();
         secTimer.stop();
         minTimer.stop();
+    }
+
+    public TimerController getInstance(){
+        return timerInst;
+    }
+
+    public int getElapsedSec(){
+        return secCounter;
+    }
+
+    public String getTime(){
+        if(h == 0){
+            return castToString(m) + " : " + castToString(s) + " : " + castToString(ms);
+        }else{
+            return "" + h + castToString(m) + " : " + castToString(s) + " : " + castToString(ms);
+        }
+    }
+
+    public String castToString(int i){
+        return String.format("%02d", i);
     }
 }
