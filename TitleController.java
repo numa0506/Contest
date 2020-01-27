3c8e3675792b8ab24321b87bb9ebe088d964a38a
Kota_Naganuma <naganuma.kota.19@shizuoka.ac.jp>
2020-01-27 15:15:04 +0900

    1/27追加差分１

--- TitleController.java
+++ TitleController.java
@@ -1,31 +1,33 @@
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
-import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.KeyCode;
-import javafx.scene.Group;
-import javafx.stage.Window;
-import java.io.IOException;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.Node;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.event.Event;
-
-public class TitleController implements Initializable {
-  @Override
-  public void initialize(URL url, ResourceBundle rb) {
-  }
-  public void onStart(){
-    MapGame.startGame();
-  }
-
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.Group;
+import javafx.stage.Window;
+import java.io.IOException;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Node;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.event.Event;
+
+public class TitleController implements Initializable {
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+  }
+  public void onStart() throws Exception{
+    try{
+      MapGame.getInstance().startGame();
+    }catch(Exception e){e.printStackTrace();}
+  }
+
 }
